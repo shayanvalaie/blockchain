55769363ec6e9a15b9a5d5c87032c5868a053b2c
Shayan Valaie <shayan@Shayans-MacBook-Pro.local>
2021-11-20 18:27:02 -0800

    initial commit

---
+++ blockchain.py
@@ -0,0 +1,16 @@
+blockchain = []
+
+
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction):
+    blockchain.append([last_transaction, transaction_amount])
+
+
+add_value(2, [1])
+add_value(.9, get_last_blockchain_value())
+add_value(10.89, get_last_blockchain_value())
+
+print(blockchain)
